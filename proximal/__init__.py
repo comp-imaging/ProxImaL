ff7ccd9cb0aff08a1d5193f80a3ea8e051fca84c
Steven Diamond <stevend2@stanford.edu>
2016-10-14 10:06:51 -0700

    Bump version: 0.1.4 → 0.1.5

--- proximal/__init__.py
+++ proximal/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .prox_fns import *
 from .lin_ops import *
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='proximal',
-    version='0.1.4',
+    version='0.1.5',
     packages=['proximal',
               'proximal.prox_fns',
               'proximal.lin_ops',
