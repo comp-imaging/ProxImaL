b43578e9ed8d431b5f6fd53c01b57fc9f45254db
Christoph Wiedemann <christoph.wiedemann@hs-weingarten.de>
2017-04-09 12:35:16 +0200

    port cuda tests to python unittests

--- proximal/tests/test_cuda_prox_fn.py
+++ proximal/tests/test_cuda_prox_fn.py
@@ -1,61 +1,60 @@
+from proximal.tests.base_test import BaseTest
+
 from proximal.prox_fns.group_norm1 import group_norm1
 from proximal.lin_ops.variable import Variable
 
 from numpy import random
 import numpy as np
 
-import pycuda.compiler
+class TestCudaProxFn(BaseTest):    
 
-pycuda.compiler.DEFAULT_NVCC_FLAGS.extend(['-ccbin', 'clang-3.8', '--prec-sqrt=true'])
-
-def check_group_norm1(eps = 1e-4):
-    x = Variable((10,10,2,3))
-    f = group_norm1(x, [2,3])
-    
-    v = np.reshape(np.arange(10*10*2*3), (10,10,2,3)).astype(np.float32)
-    xhat1 = f.prox(1, v.copy())
-    xhat2 = f.prox_cuda(1, v.copy()).get()
-    
-    if not np.all(np.abs(xhat1 - xhat2) < eps):
-        print(f.cuda_code)
-        print("failed: %f" % np.amax(np.abs(xhat1-xhat2)))
-        assert(False)
+    def test_group_norm1(self):
+        random.seed(0)
         
-    maxeps = 0
-    for i in range(50):
-        v = random.rand(10,10,2,3).astype(np.float32)
-        rho = np.abs(random.rand(1))
-        xhat1 = f.prox(rho, v.copy())
-        xhat2 = f.prox_cuda(rho, v.copy()).get()
+        x = Variable((10,10,2,3))
+        f = group_norm1(x, [2,3])
         
-        err = np.amax(np.abs(xhat1 - xhat2))
-        if not err < eps:
+        v = np.reshape(np.arange(10*10*2*3), (10,10,2,3)).astype(np.float32)
+        xhat1 = f.prox(1, v.copy())
+        xhat2 = f.prox_cuda(1, v.copy()).get()
+        
+        if not np.all(np.abs(xhat1 - xhat2) < 1e-4):
             print(f.cuda_code)
             print("failed: %f" % np.amax(np.abs(xhat1-xhat2)))
-            assert(False)
-        maxeps = max(err,maxeps)
-
-    for i in range(50):
-        v = random.rand(10,10,2,3).astype(np.float32)
-        rho = np.abs(random.rand(1))
-        alpha = np.abs(random.rand(1))
-        beta = np.abs(random.rand(1))
-        gamma = np.abs(random.rand(1))
-        c = np.abs(random.rand(*f.c.shape))
-        b = np.abs(random.rand(*f.b.shape))
-        
-        xhat1 = f.prox(rho, v.copy(), alpha=alpha, beta=beta, gamma=gamma, c=c, b=b)
-        xhat2 = f.prox_cuda(rho, v.copy()).get()
-        
-        err = np.amax(np.abs(xhat1 - xhat2))
-        if not err < eps:
-            print(f.cuda_code)
-            print("failed: %f" % np.amax(np.abs(xhat1-xhat2)))
-            assert(False)
-        maxeps = max(err,maxeps)
-
-    print("Max error: %.4e" % maxeps)
+        self.assertTrue(np.all(np.abs(xhat1 - xhat2) < 1e-4))
+            
+        eps = 1e-5
+        maxeps = 0
+        for i in range(50):
+            v = random.rand(10,10,2,3).astype(np.float32)
+            rho = np.abs(random.rand(1))
+            xhat1 = f.prox(rho, v.copy())
+            xhat2 = f.prox_cuda(rho, v.copy()).get()
+            
+            err = np.amax(np.abs(xhat1 - xhat2))
+            if not err < eps:
+                print(f.cuda_code)
+                print("failed: %f" % np.amax(np.abs(xhat1-xhat2)))
+            self.assertTrue(err < eps)
+            maxeps = max(err,maxeps)
     
-if __name__ == "__main__":
-    check_group_norm1()
-    
+        for i in range(50):
+            v = random.rand(10,10,2,3).astype(np.float32)
+            rho = np.abs(random.rand(1))
+            alpha = np.abs(random.rand(1))
+            beta = np.abs(random.rand(1))
+            gamma = np.abs(random.rand(1))
+            c = np.abs(random.rand(*f.c.shape))
+            b = np.abs(random.rand(*f.b.shape))
+            
+            xhat1 = f.prox(rho, v.copy(), alpha=alpha, beta=beta, gamma=gamma, c=c, b=b)
+            xhat2 = f.prox_cuda(rho, v.copy()).get()
+            
+            err = np.amax(np.abs(xhat1 - xhat2))
+            if not err < eps:
+                print(f.cuda_code)
+                print("failed: %f" % np.amax(np.abs(xhat1-xhat2)))
+            self.assertTrue(err < eps)
+            maxeps = max(err,maxeps)
+        
+        print(maxeps)
