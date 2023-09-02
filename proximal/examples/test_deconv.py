44eef8c95886d37f193e9a733e867910edf76c02
Antony Chan <antony.sigma@gmail.com>
2023-09-02 14:41:32 -0700

    Beautify test_deconv.py

--- proximal/examples/test_deconv.py
+++ proximal/examples/test_deconv.py
@@ -1,20 +1,17 @@
 # Proximal
 import sys
+
 sys.path.append('../../')
 
-from proximal.utils.utils import *
-from proximal.utils.metrics import *
-from proximal.halide.halide import *
-from proximal.lin_ops import *
-from proximal.prox_fns import *
-from proximal.algorithms import *
+from proximal.utils.utils import (get_test_image, get_kernel, Impl, tic, toc)
+from proximal.halide.halide import Halide
+from proximal import (conv, group_norm1, sum_squares, Problem, Variable, grad,
+                      cg_options)
 
 import numpy as np
 from scipy import ndimage
-from scipy.misc import ascent
 
 import matplotlib.pyplot as plt
-import cv2
 
 ############################################################
 
@@ -27,8 +24,7 @@ K = get_kernel(31, len(I.shape))
 
 # Generate observation
 sigma_noise = 0.01
-b = ndimage.convolve(I, K,
-                     mode='wrap')
+b = ndimage.convolve(I, K, mode='wrap')
 b += sigma_noise * np.random.randn(*I.shape)
 
 # Display data
@@ -52,36 +48,38 @@ lambda_tv = 1.0
 lambda_data = 500.0
 
 tic()
-Halide('fft2_r2c', target_shape=(WIDTH, WIDTH), recompile=True, reconfigure=True)
+Halide('fft2_r2c',
+       target_shape=(WIDTH, WIDTH),
+       recompile=True,
+       reconfigure=True)
 Halide('ifft2_c2r', recompile=True)
-print('Halide compile time = {:.0f}ms'.format(toc()))
+print(f'Halide compile time = {toc():.0f}ms')
 
 x = Variable(I.shape)
 
 options = cg_options(tol=1e-4, num_iters=100, verbose=False)
 tic()
 
-solver_options = dict(
-    max_iters=300,
-    eps_abs=1e-2,
-    eps_rel=1e-2,
-    lin_solver_options=options,
-    metric=None,
-    verbose=True,
-)
+solver_options = {
+    'max_iters': 300,
+    'eps_abs': 1e-2,
+    'eps_rel': 1e-2,
+    'lin_solver_options': options,
+    'metric': None,
+    'verbose': True,
+}
 
 hl = Impl['halide']
 
 prob = Problem(
     sum_squares(conv(K, x, dims=2, implem=hl) - b) * lambda_data +
-    lambda_tv *
-    group_norm1(grad(x, dims=2, implem=hl), [2], implem=hl),
+    lambda_tv * group_norm1(grad(x, dims=2, implem=hl), [2], implem=hl),
     implem=hl,
     lin_solver='cg',
 )
 result = prob.solve(solver='ladmm', **solver_options)
 
-print('Overall solver took: {:.1f}ms; cost function = {}'.format(toc(), result))
+print(f'Overall solver took: {toc():.1f}ms; cost function = {result}')
 
 plt.subplot(224)
 imgplot = plt.imshow(x.value,
