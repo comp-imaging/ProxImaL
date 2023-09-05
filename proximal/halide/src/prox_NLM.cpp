2867ee6143c537f62e1a7d03923dd043889b8360
Antony Chan <antony.sigma@gmail.com>
2023-09-05 13:02:34 -0700

    Cross-platform build support of non-local means algorithm
    Detect the native system for the OpenCV library. Compile `exten_NLM` as
    a dynamic library. Modernize the Halide-C++ interface that calls the
    `extern_NLM` function.

--- proximal/halide/src/prox_NLM.cpp
+++ proximal/halide/src/prox_NLM.cpp
@@ -1,47 +1,56 @@
 ////////////////////////////////////////////////////////////////////////////////
-//NLM proximal operator from "core/prox_operators.h"
+// NLM proximal operator from "core/prox_operators.h"
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <Halide.h>
 
 using namespace Halide;
-using namespace Halide::BoundaryConditions;
 
 #include "core/prox_operators.h"
 
+namespace {
+
+constexpr auto n_params = 4;
+
 class proxNLM_extern_gen : public Generator<proxNLM_extern_gen> {
-public:
+   public:
+    Input<Buffer<float, 3>> input{"input"};
+    Input<float> theta{"theta"};
+    Input<Buffer<float, 1>> params{"params"};
 
-    ImageParam input{Float(32), 3, "input"};
-    Param<float> theta{"theta"};
-    ImageParam params{Float(32), 2, "params"};
+    Output<Buffer<float, 3>> output{"output"};
 
-    Func build() {
+    void generate() {
+        const Expr width = input.width();
+        const Expr height = input.height();
+        const Expr channels = input.channels();
 
-        Expr width = input.width();
-        Expr height = input.height();
-        Expr channels = input.channels();
-     
-        //Get input and reshuffle
-        Func input_func("input_func");
-        input_func(x, y, c) = input(x, y, c);
-        
-        //Params
-        Func params_func("param_func");
-        params_func(k) = params(0,k);
+        // Schedule
+        Func NLM_input{"NLM_input"};
+        output = proxNLM(input, theta, params, width, height, channels);
 
-        //Schedule
-        Func NLM_input("NLM_input");
-        NLM_input = proxNLM(input_func, theta, params_func, width, height, channels);
-        NLM_input.compute_root();
+        input.dim(0).set_min(0).set_stride(1);
+        input.dim(1).set_min(0).set_stride(width);
+        input.dim(2).set_min(0).set_stride(width * height);
 
-        //Allow for arbitrary strides
-        input.set_stride(0, Expr());
-        params.set_stride(0, Expr());
-        NLM_input.output_buffer().set_stride(0, Expr()); 
-        
-        return NLM_input;
+        params.dim(0).set_bounds(0, n_params);
+
+        output.dim(0).set_bounds(0, width).set_stride(1);
+        output.dim(1).set_bounds(0, height).set_stride(width);
+        output.dim(2).set_bounds(0, channels).set_stride(width * height);
+    }
+
+    void schedule() {
+        if (using_autoscheduler()) {
+            input.set_estimates({{0, 512}, {0, 512}, {0, 1}});
+            output.set_estimates({{0, 512}, {0, 512}, {0, 1}});
+            params.set_estimates({{0, n_params}});
+            theta.set_estimate(1.0f);
+            return;
+        }
     }
 };
 
-auto proxNLMextern = RegisterGenerator<proxNLM_extern_gen>("proxNLM_extern_gen");
+}  // namespace
+
+HALIDE_REGISTER_GENERATOR(proxNLM_extern_gen, proxNLM);
