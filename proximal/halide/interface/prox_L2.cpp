6a8dec89534554e4ed28779710c1108b2b3fdc8d
Antony Chan <antony.sigma@gmail.com>
2023-09-13 14:57:49 -0700

    Expose least_square_direct algorithm to Python
    Assume zero image offsets. Marshal the Numpy buffer to the Halide
    accelerated pipeline.

---
+++ proximal/halide/interface/prox_L2.cpp
@@ -0,0 +1,26 @@
+#include "least_square_direct.h"
+#include "util.hpp"
+
+namespace proximal {
+
+int
+prox_L2_glue(const array_float_t input, const float theta, const array_float_t offset,
+             const array_cxfloat_t freq_diag, const array_float_t output) {
+    auto input_buf = getHalideBuffer<3>(input);
+    auto offset_buf = getHalideBuffer<3>(offset);
+    auto freq_diag_buf = getHalideComplexBuffer<4>(freq_diag);
+    auto output_buf = getHalideBuffer<3>(output, true);
+
+    constexpr auto zero_offset = 0;
+
+    const auto success = least_square_direct(input_buf, zero_offset, zero_offset, theta, offset_buf,
+                                             freq_diag_buf, output_buf);
+    output_buf.copy_to_host();
+    return success;
+}
+
+}  // namespace proximal
+
+PYBIND11_MODULE(prox_L2, m) {
+    m.def("run", &proximal::prox_L2_glue, "Least square algorithm with direct FFT method");
+}
---
+++ proximal/halide/interface/prox_L2_ignore_offset.cpp
@@ -0,0 +1,26 @@
+#include "least_square_direct_ignore_offset.h"
+#include "util.hpp"
+
+namespace proximal {
+
+int
+prox_L2_ignore_offset_glue(const array_float_t input, 
+             const array_cxfloat_t freq_diag, const array_float_t output) {
+    auto input_buf = getHalideBuffer<3>(input);
+    auto freq_diag_buf = getHalideComplexBuffer<4>(freq_diag);
+    auto output_buf = getHalideBuffer<3>(output, true);
+
+    constexpr auto dont_care = 0;
+    auto& dont_care_buf = input_buf;
+
+    const auto success = least_square_direct_ignore_offset(input_buf, dont_care, dont_care, dont_care, dont_care_buf,
+                                             freq_diag_buf, output_buf);
+    output_buf.copy_to_host();
+    return success;
+}
+
+}  // namespace proximal
+
+PYBIND11_MODULE(prox_L2_ignore_offset, m) {
+    m.def("run", &proximal::prox_L2_ignore_offset_glue, "Least square algorithm with direct FFT method");
+}
